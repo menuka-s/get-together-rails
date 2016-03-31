e24d306e5bed72627b6c4e09fed8f535e11a099c
Lydia & Michael Pintar <pair+lydiamnash.thepintar@devbootcamp.com>
2016-03-31 18:23:18 -0500

    Start login functionality

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,8 @@
 module UsersHelper
+
+  def current_user
+    if session[:user_id]
+      User.find(session[:user_id])
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'users#index'
 
+  resource :users
+
 end
