fd560778fa62b425443451f28e68b20e090f0968
Lydia <email": null>
2016-04-04 16:58:18 -0500

    Add new activity page

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,11 +1,20 @@
 class ActivitiesController < ApplicationController
 
   def new
-
+    @activity = Activity.new
   end
 
   def create
-
+    @activity = Activity.new(activity_params)
+    if @activity.save
+      redirect_to new_event_path
+    else
+      render new_activity_path(@activity)
+    end
   end
 
+  private
+  def activity_params
+    params.require(:activity).permit(:name)
+  end
 end
