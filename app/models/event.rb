5d30f42ea250f61d9118a2d81fff4e23b455e45e
sfinkenhdz <sarah.finken@gmail.com>
2016-04-04 13:40:26 -0500

    Can't join full events

--- app/models/event.rb
+++ app/models/event.rb
@@ -30,9 +30,10 @@ class Event < ActiveRecord::Base
   end
 
   def open?
-    if self.joined_users <= self.max_participants
-      true
-    end
+    self.joined_users.to_a.length < self.max_participants ? true : false
+  end
+
+
 
 
   def push_notification
