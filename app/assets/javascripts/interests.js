242d6a57cde2049bc2af73a34ec81cb2d10fa881
Lydia & Michael Pintar <pair+lydiamnash.thepintar@devbootcamp.com>
2016-04-04 08:48:42 -0500

    add autofill

--- app/assets/javascripts/interests.js
+++ app/assets/javascripts/interests.js
@@ -1,5 +1,23 @@
   $(document).ready(function(){
+    var userActivities = $("#user-activities").text();
+    var activitiesArray = userActivities.split(',');
     $("#activity").keyup(function(){
-      console.log($("#activity").val())
+      $("#suggestions-container").empty();
+      var userInput = $("#activity").val();
+      var searchString = new RegExp(userInput, "gi");
+      if($("#suggestions-container").css("display")=="none"){
+        $("#suggestions-container").slideToggle("slow");
+        console.log($("#activity").val())
+      }
+      activitiesArray.forEach(function(activity){
+
+        if (activity.match(searchString) != null){
+          $("#suggestions-container").append("<div class = 'activity-suggestion'>" + activity + "</div>")
+        }
+      })
     });
+    $("#suggestions-container").on("click", ".activity-suggestion", function(){
+      $("#suggestions-container").slideToggle("slow");
+    });
+
   });
