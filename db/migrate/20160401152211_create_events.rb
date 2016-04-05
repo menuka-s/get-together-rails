6b1d39727cd1730d8dbaa650ac297401c6a82f06
Ian Kinner & Menuka Samaranayake <pair+ian-kinner.menuka-s@devbootcamp.com>
2016-04-05 15:50:40 -0500

    Change commenter link to local profile not FB

--- db/migrate/20160401152211_create_events.rb
+++ db/migrate/20160401152211_create_events.rb
@@ -2,14 +2,14 @@ class CreateEvents < ActiveRecord::Migration
   def change
     create_table :events do |t|
       t.text  :name, null: false
-      t.text :description#, null: false
+      t.text :description, null: false
       t.integer :creator_id, null: false
-      t.datetime :date#, null: false
+      t.datetime :date, null: false
       t.string :location_name
-      t.string :address#, null: false
+      t.string :address, null: false
       t.float :latitude
       t.float  :longitude
-      t.integer :activity_id#, null: false
+      t.integer :activity_id, null: false
       t.integer :max_participants
 
 
