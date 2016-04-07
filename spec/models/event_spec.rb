1590c9e67eca18f3e4d1deb7117708985cded7e5
Lydia <email": null>
2016-04-07 13:44:51 -0500

    add skeleton for testing the event model

---
+++ spec/models/event_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+describe Event do
+
+  let(:event) { Event.new }
+
+# This method is never used in the application?
+  pending 'self#event_locations' do
+    xit '' do
+
+    end
+  end
+
+  describe '#open_spots' do
+    xit 'returns the number of open spots an event has' do
+
+    end
+  end
+
+  describe '#open?' do
+    xit 'returns true if there are open spots in an event' do
+
+    end
+
+    xit 'returns false if there no open spots in an event' do
+
+    end
+  end
+
+  describe '#distance_to_user' do
+    xit 'returns the distance in miles between an event and the current user' do
+
+    end
+  end
+end
+
+
+
